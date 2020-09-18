225358046da8d025a72d4502a176e7a95a2647cc
torelochka <zheleznov213@gmail.com>
2020-09-18 16:43:40 +0500

    delete index.jsp

--- src/main/java/ru/itis/witchCrutch/TestController.java
+++ src/main/java/ru/itis/witchCrutch/TestController.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 public class TestController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello World!");
+        resp.getWriter().write("Hello world!");
     }
 }
